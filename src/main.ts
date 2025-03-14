f9dbc97bd7180c3b6cb1b04e56f3b366b681904e
unknown <natan.br15@gmail.com>
2025-03-14 11:26:31 -0300

    :sparkles: mongodb integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: "http://localhost:3001"
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
