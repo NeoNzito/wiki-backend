540666347b0ce1ee7bc784ff7fefc962c53d3e74
unknown <natan.br15@gmail.com>
2025-03-20 11:25:05 -0300

    :sparkles: auth module, jwt authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,31 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { UserModule } from "src/user/user.module";
 import { jwtConstants } from "./jwt/constants";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { APP_GUARD } from "@nestjs/core";
+import { AuthGuard } from "./jwt/guard/auth.guard";
 
 
 @Module({
- imports: [
-    UserModule,
-    JwtModule.register({
-        global: true,
-        secret: jwtConstants.secret,
-        signOptions: {
-            expiresIn: jwtConstants.accessExpiresIn
+    imports: [
+        UserModule,
+        JwtModule.register({
+            global: true,
+            secret: jwtConstants.secret,
+            signOptions: {
+                expiresIn: jwtConstants.accessExpiresIn
+            },
+        })
+    ],
+    controllers: [AuthController],
+    providers: [
+        AuthService,
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard
         },
-    })
- ],
- providers: [],
- exports: []
+    ],
+    exports: [AuthService]
 })
 export class AuthModule {}
