36817b1d1b4192e0df822deeec51e9a92430b80f
NeoNzito <natan.br15@gmail.com>
2025-03-26 09:04:05 -0300

    :sparkles: new microservices

---
+++ src/ms-moderation/main.ts
@@ -0,0 +1,19 @@
+// LangChain Gemini
+
+import { NestFactory } from "@nestjs/core";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { AppModule } from "src/app.module";
+
+async function bootstrap() {
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.RMQ,
+        options: {
+            urls: ["amqp://localhost:5672"],
+            queue: "moderation_queue"
+        }
+    });
+
+    await app.listen();
+}
+
+bootstrap();
---
+++ src/ms-notification/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from "@nestjs/core";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { AppModule } from "src/app.module";
+
+async function bootstrap() {
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.RMQ,
+        options: {
+            urls: ["amqp://localhost:5672"],
+            queue: "notification_queue",
+        }
+    });
+
+    await app.listen();
+}
+
+bootstrap();
