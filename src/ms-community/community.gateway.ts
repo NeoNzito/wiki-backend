e4a1628b2cfd398ab381e2213b010230129cef3d
NeoNzito <natan.br15@gmail.com>
2025-03-31 11:22:19 -0300

    :sparks: community gateway

---
+++ src/ms-community/community.gateway.ts
@@ -0,0 +1,30 @@
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+
+
+@WebSocketGateway({ cors: true })
+export class NotificationGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer()
+    server: Server;
+
+    private users = new Map<string, string>();
+
+    handleDisconnect(client: any) {
+    }
+
+    handleConnection(client: any, ...args: any[]) {
+    }
+
+    @SubscribeMessage("subscribeToCommunity")
+    handleSubscribe(client: Socket, userId: string) {
+        this.users.set(client.id, userId);
+    }
+
+    sendComment(userId: string, message: string) {
+        for (const [clientId, storedUserId] of this.users.entries()) {
+            if (storedUserId === userId) {
+                this.server.to(clientId).emit('newComment', message);
+            }
+        }
+    }
+}
